d12f46c39c93b1e4c44b6aa2cb03af9e646a4e8a
slingod <slingo.drisca1@gmail.com>
2025-03-10 16:12:20 +0100

    add Admin for Test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   # Devise modules
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  # Vérifie si l'utilisateur est admin
+  def admin?
+    self.admin
+  end
 end
